5f6a96bf48e3ce4566e4c3024a668ce679982a2f
Xavier Boubert <xavier@boubert.fr>
2015-07-13 19:59:35 +0200

    fix(common express gulpfile): adapt to the waterline service

--- features/controllers/resources/common-express-1000-gulpfile.js
+++ features/controllers/resources/common-express-1000-gulpfile.js
@@ -42,92 +42,58 @@ module.exports = function expressTask(gulp, tasksBefore) {
   gulp.task('express', tasksBefore.concat(['less', 'minify', 'html']), function() {
 
     require(path.resolve(__dirname, 'models/common-i18n-model'))();
-    require(path.resolve(__dirname, 'models/common-mongo-service'))();
-    require(path.resolve(__dirname, 'models/common-sql-service'))();
     require(path.resolve(__dirname, 'models/common-abstract-model'))();
     require(path.resolve(__dirname, 'models/common-abstract-service'))();
+    require(path.resolve(__dirname, 'models/common-database-service'))();
     require(path.resolve(__dirname, 'models/common-sockets-service'))();
 
-    var countMongo = parseInt(process.env.MONGO_COUNT || 0, 10);
+    var $DatabaseService = DependencyInjection.injector.controller.get('$DatabaseService');
+    $DatabaseService.initModels(function() {
 
-    if (countMongo) {
-      var $MongoService = DependencyInjection.injector.controller.get('$MongoService');
+      var server = express();
 
-      Array.apply(null, {length: countMongo}).map(Number.call, Number).forEach(function(i) {
-        DependencyInjection.service('$Mongo' + process.env['MONGO_NAME_' + i], [function() {
-          return $MongoService.create(
-            process.env['MONGO_HOST_' + i],
-            process.env['MONGO_DB_' + i],
-            process.env['MONGO_USER_' + i],
-            process.env['MONGO_PASSWORD_' + i],
-            process.env['MONGO_POOL_SIZE_' + i]
-          );
-        }]);
+      DependencyInjection.service('$express', [function() {
+        return express;
+      }]);
+
+      DependencyInjection.service('$server', [function() {
+        return server;
+      }]);
+
+      server.use(bodyParser.urlencoded({extended: true}));
+      server.use(compression());
+      server.use(cookieParser(process.env.COOKIE_SECRET));
+
+      server.set('port', process.env.PORT);
+
+      var http = require('http').Server(server),
+          io = require('socket.io')(http);
+
+      io.use(socketIoCookieParser(process.env.COOKIE_SECRET));
+
+      DependencyInjection.service('$http', [function() {
+        return http;
+      }]);
+
+      DependencyInjection.service('$io', [function() {
+        return io;
+      }]);
+
+      expressFiles.sort(_sortByFilePriority);
+
+      expressFiles.forEach(function(file) {
+        DependencyInjection.injector.controller.invoke(null, require(file));
       });
-    }
 
-    var countSQL = parseInt(process.env.SQL_COUNT || 0, 10);
+      socketioFiles.sort(_sortByFilePriority);
 
-    if (countSQL) {
-      var $SQLService = DependencyInjection.injector.controller.get('$SQLService');
-
-      Array.apply(null, {length: countSQL}).map(Number.call, Number).forEach(function(i) {
-        DependencyInjection.service('$SQL' + process.env['SQL_NAME_' + i], [function() {
-          return $SQLService.create(
-            process.env['SQL_TYPE_' + i],
-            process.env['SQL_HOST_' + i],
-            process.env['SQL_DB_' + i],
-            process.env['SQL_USER_' + i],
-            process.env['SQL_PASSWORD_' + i],
-            process.env['SQL_POOL_SIZE_' + i]
-          );
-        }]);
+      socketioFiles.forEach(function(file) {
+        DependencyInjection.injector.controller.invoke(null, require(file));
       });
-    }
 
-    var server = express();
-
-    DependencyInjection.service('$express', [function() {
-      return express;
-    }]);
-
-    DependencyInjection.service('$server', [function() {
-      return server;
-    }]);
-
-    server.use(bodyParser.urlencoded({extended: true}));
-    server.use(compression());
-    server.use(cookieParser(process.env.COOKIE_SECRET));
-
-    server.set('port', process.env.PORT);
-
-    var http = require('http').Server(server),
-        io = require('socket.io')(http);
-
-    io.use(socketIoCookieParser(process.env.COOKIE_SECRET));
-
-    DependencyInjection.service('$http', [function() {
-      return http;
-    }]);
-
-    DependencyInjection.service('$io', [function() {
-      return io;
-    }]);
-
-    expressFiles.sort(_sortByFilePriority);
-
-    expressFiles.forEach(function(file) {
-      DependencyInjection.injector.controller.invoke(null, require(file));
-    });
-
-    socketioFiles.sort(_sortByFilePriority);
-
-    socketioFiles.forEach(function(file) {
-      DependencyInjection.injector.controller.invoke(null, require(file));
-    });
-
-    http.listen(server.get('port'), function() {
-      console.log('► SERVER IS RUNNING ON :' + process.env.PORT);
+      http.listen(server.get('port'), function() {
+        console.log('► SERVER IS RUNNING ON :' + process.env.PORT);
+      });
     });
   });
 
