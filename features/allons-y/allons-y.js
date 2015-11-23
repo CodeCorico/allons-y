3309620dc503564d2b4c1bfe745df48c31b52d70
Xavier Boubert <xavier@boubert.fr>
2015-11-23 14:10:29 +0100

    feat(allons-y): inject *allons-y-bootstrap.js modules before start and stop

--- features/allons-y/allons-y.js
+++ features/allons-y/allons-y.js
@@ -2,7 +2,8 @@
 
 GLOBAL.DependencyInjection = require('mvw-injection').MVC();
 
-var path = require('path');
+var path = require('path'),
+    async = require('async');
 
 module.exports = new (function() {
 
@@ -10,18 +11,49 @@ module.exports = new (function() {
 
   var _this = this;
 
-  this.start = function() {
-    console.log('START');
-
-    // DependencyInjection
-
+  function _bootstrap(options, callback) {
     var files = _this.filesSyncSortered('*allons-y-bootstrap.js');
 
-    console.log(files);
+    async.mapSeries(files, function(file, nextFile) {
+
+      var bootstrapModule = require(path.resolve(file));
+
+      bootstrapModule.bootstrap(options, nextFile);
+
+    }, function() {
+      callback();
+    });
+  }
+
+  this.start = function() {
+    console.log('BEFORE START');
+
+    _bootstrap({
+      owner: 'start'
+    }, function() {
+
+      console.log('START');
+
+      var files = _this.filesSyncSortered('*allons-y-start.js');
+
+      files.forEach(function(file) {
+        var startModule = require(path.resolve(file));
+
+
+        DependencyInjection.injector.controller.invoke(null, startModule.module);
+      });
+    });
   };
 
   this.stop = function() {
-    console.log('STOP');
+    console.log('BEFORE STOP');
+
+    _bootstrap({
+      owner: 'stop'
+    }, function() {
+
+      console.log('STOP');
+    });
   };
 
 })();
